eacf8d4bc59701121ec68d12c3500341f86153f0
Milan Lamote <milanlamote@hotmail.com>
2016-11-08 16:07:02 +0100

    Basic chatprogram works, still see own messages.

--- app.rb
+++ app.rb
@@ -1,15 +1,27 @@
 require 'rubygems'
 require 'mqtt'
 
-# Publish example
-MQTT::Client.connect('10.177.33.144') do |c|
-  c.publish('test', 'message')
+puts  'Who are you?'
+@name = gets.chomp
+
+
+Thread.new do
+  while true
+      puts  'Enter messages:'
+      text = gets
+      # Publish example
+      MQTT::Client.connect('10.177.33.144') do |c|
+        c.publish('BABL', @name + ': ' + text)
+      end
+  end
 end
+
+
 
 # Subscribe example
 MQTT::Client.connect('10.177.33.144') do |c|
   # If you pass a block to the get method, then it will loop
-  c.get('test') do |topic,message|
+  c.get('BABL') do |topic,message|
     puts "#{topic}: #{message}"
   end
 end
